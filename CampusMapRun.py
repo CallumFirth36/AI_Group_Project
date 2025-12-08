2c8cdf2aff72c8d6c8618cb5508b9bdf8ff43b4a
Skips2003 <jonnyforbes0805@gmail.com>
2025-12-08 17:43:06 +0000

    Add CampusMapRun.py and randomSearchBot.py
    random search bot selects a random direction to move in, will maybe find the target goal after the heat death of the universe.
    
    CampusMapRun is a very basic implication for interacting with the environment

---
+++ CampusMapRun.py
@@ -0,0 +1,15 @@
+import sys 
+sys.path.append('../')
+from MapTraversal import MapTraversalEnvironment
+from randomSearchBot import RandomSearchBot
+
+you = RandomSearchBot()
+env = MapTraversalEnvironment(renderMode="human")
+
+obs = env.getObs(False)
+while not obs[3]:
+    action = you.move(obs)
+    obs = env.step(action)
+    env.render()
+    if(obs[3]):
+        obs = env.reset()

48d98d9e00e8628856e314325301694e893f5000
Skips2003 <jonnyforbes0805@gmail.com>
2025-12-08 17:44:31 +0000

    Update CampusMapRun.py

--- CampusMapRun.py
+++ CampusMapRun.py
@@ -1,6 +1,6 @@
 import sys 
 sys.path.append('../')
-from MapTraversal import MapTraversalEnvironment
+from CampusTraversalEnvironment import MapTraversalEnvironment
 from randomSearchBot import RandomSearchBot
 
 you = RandomSearchBot()

4eb366837553533365b15749c682eb10c1df017d
Skips2003 <jonnyforbes0805@gmail.com>
2025-12-11 18:20:14 +0000

    Update CampusTraversalEnvironment.py
    Im sorry for changing this entire file all at once but this works with the update RL model based on Callums which I am also uploading and I dont want to make mistakes changing this doc line by line

--- CampusTraversalEnvironment.py
+++ CampusTraversalEnvironment.py
@@ -5,225 +5,192 @@
 # Jonny Forbes 2643497
 # Bailey Clark 2636229
 # firstName lastName MatriculationNumber
+## The implementation of the RL machine environment learning from gymnasium
 
 from enum import Enum
-import time
 import gymnasium as gym
 from gymnasium import spaces
-import numpy as np # Library for numerical computing
+import numpy as np 
 from gymnasium.envs.registration import register
 import pygame
 
-WIDTH = 1280
-HEIGHT = 720
-IMAGE_SIZE = (WIDTH, HEIGHT)
-ROADCOLOURS = (200, 214, 226), (197,212,225)
-LOCATIONS = [] # Add Colours for building locations here (Probably something simple like (1,0,0) an increminting the first digit each time, I will add a key somewhere on the Git showing which building each value represents!
-PATHVALUE = 1
-
-
-# movement directions
 class Actions(Enum):
-    EAST = 0
-    NORTH = 1
-    WEST = 2
-    SOUTH = 3
+    RIGHT = 0
+    UP = 1
+    LEFT = 2
+    DOWN = 3
 
-# custom enviroment
 class MapTraversalEnvironment(gym.Env):
-    metadata = {"renderModes": ["human", "rgb_array"], "renderFPS": 50}
+    metadata = {"render_modes": ["human", "rgb_array"], "render_fps": 50}
 
-    #initialize enviroment
     def __init__(self, renderMode=None):
-        # Image Size contains resolution of image "campusMapNoEntrances.png"
-        self.size = IMAGE_SIZE
-        self.window = pygame.display.set_mode(self.size)
-        self.mapImage = pygame.image.load("campusMapAllRoadGreenLine.png").convert()
-        self.map = np.zeros(self.size)
-        self.generateGridValues()
-        self.distanceTraversed = 0
-        self.reward = 0
-        self.currentLocation = [0,0]
-        self.targetLocation = [0,0]
-        self.randomStartAndTarget()
-
-        assert renderMode is None or renderMode in self.metadata["renderModes"]
+        pygame.init()
+        
+        self.width = 1280
+        self.height = 720
+        self.size = (1280, 720)
         self.renderMode = renderMode
-        self.clock = None
+        self.window = None
 
-        # observation space - just the grid, coordinates of current/target location and value of current/target location (Change value of high to match total different types of values in grid)- can make copy of grid inside of agent and do calculations there
+        if self.renderMode == "human":
+            self.window = pygame.display.set_mode(self.size)
+
+        self.filename = "campusMapAllRoadGreenLine.png"
+            
+        self.mapSurface = pygame.image.load(self.filename)
+        
+        if self.window is not None:
+            self.mapImage = self.mapSurface.convert()
+        else:
+            self.mapImage = self.mapSurface
+
+        self.map = np.zeros((self.width, self.height))
+        self.generateGridValues()
+        
+        self.currentLocation = np.array([0, 0])
+        self.targetLocation = np.array([0, 0])
+        
+        # Observation Space
         self.observation_space = spaces.Dict({
-            "mapGrid": spaces.Box(low=0, high=self.size, shape=(self.size), dtype = np.int32),
-            "currentLocation": spaces.Box(low=(0,0), high=self.size, shape=(2, ), dtype = np.int32),
-            "targetLocation": spaces.Box(low=(0,0), high=self.size, shape=(2, ), dtype = np.int32),
+            "agent": spaces.Box(0, max(self.width, self.height), shape=(2,), dtype=int),
+            "target": spaces.Box(0, max(self.width, self.height), shape=(2,), dtype=int),
         })
 
-        # 9 actions: "north", "south", "east", "west", "northwest", "northeast", "southwest", "southeast", "enter building"
-        self.action_space = spaces.Discrete(9)
+        self.action_space = spaces.Discrete(5)
 
-        self.actionToDirection = {
-            Actions.EAST.value: np.array([1, 0]),
-            Actions.NORTH.value: np.array([0, 1]),
-            Actions.WEST.value: np.array([-1, 0]),
-            Actions.SOUTH.value: np.array([0, -1]),
-            Actions.NORTHWEST.value: np.array([-1, 1]),
-            Actions.NORTHEAST.value: np.array([1, 1]),
-            Actions.SOUTHEAST.value: np.array([-1, -1]),
-            Actions.SOUTHWEST.value: np.array([1, -1]),
-            8: np.array([0, 0])
+        # Action Space
+        self._action_to_direction = {
+            Actions.RIGHT.value: np.array([1, 0]),
+            Actions.UP.value: np.array([0, -1]), 
+            Actions.LEFT.value: np.array([-1, 0]),
+            Actions.DOWN.value: np.array([0, 1]),
+            4: np.array([0, 0])
         }
 
-        assert renderMode is None or renderMode in self.metadata["renderModes"]
-        self.renderMode = renderMode
-        self.clock = None
-
-    # returns current observation of enviroment
+    # Get Observations from environment
     def getObs(self):
+        return {"agent": self.currentLocation, "target": self.targetLocation}
+    
+    # Get Distance to goal
+    def getInfo(self):
+        return {
+            "distance": np.linalg.norm(
+                self.currentLocation - self.targetLocation, ord=1
+            )
+        }
 
-    # Janky and needs change to actually use observation space but this works for now
-        return self.map, self.currentLocation, self.targetLocation, hasArrived
-
-    # resets enviroment to initial state
+    # Reset
     def reset(self, seed=None, options=None):
-        #Grid values do not need to be reset only the dispay image and new start/targets
-        #we need to add the building locations to the Map as their own colours adn use them as the target locations not jsut random postions on green line, I will do that soon!
         super().reset(seed=seed)
-        self.mapImage = pygame.image.load("campusMapAllRoadGreenLine.png").convert()
+        
+        # Reset the output pygame map
+        if self.window is not None:
+             self.mapImage = self.mapSurface.convert()
+        else:
+             self.mapImage = self.mapSurface
+
+        self.currentLocation = np.array([0, 0])
+        self.targetLocation = np.array([0, 0])
+        
+        # Set random start and goal
         self.randomStartAndTarget()
 
-        return self.getObs(False)
+        return self.getObs(), self.getInfo()
 
-    #Set starting location
+    # Set start 
     def setStart(self, startPos):
-        self.currentLocation[0] = startPos[0]
-        self.currentLocation[1] = startPos[1]
-        return True
+        self.currentLocation = np.array(startPos)
 
-    
-    #Set targetLocation
+    # Set goal and draw marker on map
     def setTarget(self, targetPos):
-        self.targetLocation[0] = targetPos[0]
-        self.targetLocation[1] = targetPos[1]
-        return True
+        self.targetLocation = np.array(targetPos)
+        self.drawMarker(self.targetLocation)
+    
+    # Draw blue marker on map
+    def drawMarker(self, location):
+        x, y = location
+        for i in range(-2, 3):
+            for j in range(-2, 3):
+                if -1 < x+i < self.width and -1 < y+j < self.height:
+                    self.mapImage.set_at((x+i, y+j),(0, 0, 255))
 
-    #Maybe used in future for allowing the user to click anywhere on the map for start and end? IDK might be difficult
-    def setStartAndEndMouse(self):
-        for event in pygame.event.get():
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                posx = pygame.mouse.get_pos()[0]    
-                if not self.setStart(posx):
-                    if self.setTarget(posx):
-                        break
-
-    # return True or False
-    # Ensures search methods/Neural Networks cannot generate bad movements, Add negative reward here for training nueral network to make sure it never makes wrong moves.
-    def validateMove(self, action):
-        if self.map[self.currentLocation[0] + action[0]][self.currentLocation[1] + action[1]] == 1:
-            return True
-            
-        return False
-        
-    # Very messy but sets a random location on the green line as the start and end point
-    # Also updates the map and adds a blue square for where the end point is!
+    # Set random start and goal
     def randomStartAndTarget(self):
-        targetPos = [1,0]
-        startPos = [1,0]
+        valid_pixels = np.argwhere(self.map == 1)
 
-        while self.map[startPos[0]][startPos[1]] != 1:
-            startPos = [np.random.randint(0, WIDTH), np.random.randint(0, HEIGHT)]
-        while self.map[targetPos[0]][targetPos[1]] != 1:
-            targetPos = [np.random.randint(0, WIDTH), np.random.randint(0, HEIGHT)]
-
-        print(startPos,"startPos")
-        print(targetPos,"targetPos")
-        
+        indices = np.random.choice(len(valid_pixels), 2, replace=False)
+        startPos = valid_pixels[indices[0]]
+        targetPos = valid_pixels[indices[1]]
         self.setStart(startPos)
         self.setTarget(targetPos)
-        self.mapImage.set_at((self.targetLocation[0],self.targetLocation[1]) , (0,0,255))
-        self.mapImage.set_at((self.targetLocation[0]+1,self.targetLocation[1]) , (0,0,255))
-        self.mapImage.set_at((self.targetLocation[0]-1,self.targetLocation[1]) , (0,0,255))
-        self.mapImage.set_at((self.targetLocation[0],self.targetLocation[1]+1) , (0,0,255))
-        self.mapImage.set_at((self.targetLocation[0],self.targetLocation[1]-1) , (0,0,255))
-        self.mapImage.set_at((self.targetLocation[0]+1,self.targetLocation[1]+1) , (0,0,255))
-        self.mapImage.set_at((self.targetLocation[0]+1,self.targetLocation[1]-1) , (0,0,255))
-        self.mapImage.set_at((self.targetLocation[0]-1,self.targetLocation[1]+1) , (0,0,255))
-        self.mapImage.set_at((self.targetLocation[0]-1,self.targetLocation[1]-1) , (0,0,255))
 
+    # Validate Move
+    def validateMove(self, newLoc):
+        if 0 <= newLoc[0] < self.width and 0 <= newLoc[1] < self.height:
+            if self.map[newLoc[0]][newLoc[1]] == 1:
+                return True
+        return False
 
-    # update current location and distance traversed
-    def processMove(self, action):
-        # Updates current location and changes colour on map to red, actually looks nice!
-        # I have included distance traversed counter incaser we want to display the distance on the end screen
-        self.currentLocation[0] = self.currentLocation[0] + action[0]
-        self.currentLocation[1] = self.currentLocation[1] + action[1]
-        self.drawPath()
-        self.distanceTraversed += 1
-
-    # set values of grid based on colours
     def generateGridValues(self):
-        # Simplified version of generate grid
-        # Old one was good and worked but i think this one is easier to expand into adding the locations
-        # once locations are added to the map i will add a loop that will check a list containing their colours and update the self.map grid with unique numbers
-        for x in range(WIDTH):
-            for y in range(HEIGHT):
-                pixelColour = self.mapImage.get_at((x, y))
-                if pixelColour == (0,255,0):
-                    self.map[x][y] = PATHVALUE
-    
-    # check if current location = target and return True or False
-    def hasArrived():
-        if self.currentLocation == self.targetLocation
-            return True
-      return False
+        for x in range(self.width):
+            for y in range(self.height):
+                pixelColour = self.mapSurface.get_at((x, y))
+                if pixelColour == (0, 255, 0):
+                    self.map[x][y] = 1
 
-    # updates agent in enviroment
-    # Probably needs changing but works for now
     def step(self, action):
-        self.renderFrame()
-        if(self.validateMove(action)):
-            self.processMove(action)
-
-        if(self.hasArrived()):
-            self.renderEndScreen()
-            return self.getObs(True)
-
-        return self.getObs(False)
-
-    # We should change wait for render depening on which agent is navigating the map, Could be a cool way to display the differences between them
-    def waitForRender(self):
-        time.sleep(0)
-
-    # renders current state if enviroment
-    def render(self):
-        if self.render_mode == "rgb_array":
-            return self.renderFrame()
-    
-    # render a frame
-    # self.mapImage is a png converted into a pygame surface
-    def renderFrame(self):
-        if self.window is None and self.renderMode == "human":
-            pygame.init()
-            self.window.blit(self.mapImage, self.window.get_rect())
-            pygame.display.update()
-            self.waitForRender()
+        reward = -1 
+        terminated = False
+        truncated = False
+        
+        # Check Distance to goal when entering building and set reward
+        if action == 4:
+            # Don't really know how this works but stackoverflow told me it gets the difference between two coordinates using numpys linear algebra libs
+            dist = np.linalg.norm(self.currentLocation - self.targetLocation)
+            if dist < 10.0:
+                reward = 100
+                terminated = True
+            else:
+                reward = -10
+        else:
+            direction = self._action_to_direction[action]
+            new_loc = self.currentLocation + direction
             
-        if self.clock is None and self.renderMode == "human":
-            self.clock = pygame.time.Clock()
+            if self.validateMove(new_loc):
+                self.currentLocation = new_loc
+                self.drawPath()
+            else:
+                reward = -2 
 
-        pygame.display.update()
-        self.waitForRender()
+        observation = self.getObs()
+        info = self.getInfo()
+        
+        if self.renderMode == "human":
+            self.renderFrame()
+            
+        return observation, reward, terminated, truncated, info
     
-    def drawPath():
-        for x in range(WIDTH):
-            self.mapImage.set_at((self.currentLocation[0],self.currentLocation[1]) , (255,0,0))
+    # Render frame
+    def renderFrame(self):
+        if self.window:
+            pygame.event.pump()
+            self.window.blit(self.mapImage, (0, 0))
+            pygame.display.flip()
+
+    # Draw redLine for where model has gone
+    def drawPath(self):
+        x, y = self.currentLocation
+        if 0 <= x < self.width and 0 <= y < self.height:
+            self.mapImage.set_at((x, y), (255, 0, 0))
 
     def close(self):
         if self.window is not None:
             pygame.display.quit()
             pygame.quit()
+            self.window = None
 
-# registers enviroment
 register(
-    id="GroupProject-v0", # unique name for your env
-    entry_point="GroupProjectRL:ProjectEnv", # module:class path
+    id="GroupProject-v0",
+    entry_point="MapTraversal:MapTraversalEnvironment",
+    max_episode_steps=2000,
 )
